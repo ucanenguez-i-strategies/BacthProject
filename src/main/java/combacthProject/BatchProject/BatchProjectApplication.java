211838816cd21d0887b7589dcdf2296c70ee7704
ucanenguez-i-strategies <76168240+ucanenguez-i-strategies@users.noreply.github.com>
2021-11-25 15:08:04 -0600

    first commit

---
+++ src/main/java/combacthProject/BatchProject/BatchProjectApplication.java
@@ -0,0 +1,13 @@
+package combacthProject.BatchProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/combacthProject/BatchProject/BatchProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package combacthProject.BatchProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
