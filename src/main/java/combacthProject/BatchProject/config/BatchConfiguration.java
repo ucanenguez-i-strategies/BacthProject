bddc12dca1db1f429229fc9d4e54b6ffa80f34c9
ucanenguez-i-strategies <76168240+ucanenguez-i-strategies@users.noreply.github.com>
2021-11-26 14:55:32 -0600

    Se agrego lectura de archivos csv

--- src/main/java/combacthProject/BatchProject/config/BatchConfiguration.java
+++ src/main/java/combacthProject/BatchProject/config/BatchConfiguration.java
@@ -9,13 +9,20 @@ import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.batch.item.file.transform.LineTokenizer;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
 
 import combacthProject.BatchProject.listener.HwJobExecutionListener;
 import combacthProject.BatchProject.listener.HwStepExecutioListener;
+import combacthProject.BatchProject.model.Product;
 import combacthProject.BatchProject.processor.inMemItemProcessor;
 import combacthProject.BatchProject.reader.InMemReader;
 import combacthProject.BatchProject.writer.ConsoleItemWriter;
@@ -56,8 +63,7 @@ public class BatchConfiguration {
 	public Step step2() {
 		return steps.get("step2")
 				.<Integer,Integer>chunk(3)
-				.reader(reader())
-				.processor(MenItemProcessor)
+				.reader(flatFileItemReader())
 				.writer(new ConsoleItemWriter())
 				.build();
 	}
@@ -73,11 +79,46 @@ public class BatchConfiguration {
 	}
 	
 	@Bean
+	public FlatFileItemReader flatFileItemReader() {
+		FlatFileItemReader reader = new FlatFileItemReader();
+		// step 1 permite leer y saber donde esta el archivo
+		reader.setResource(new FileSystemResource("input/product.csv"));
+		
+		//Crea el liner mapper
+		reader.setLineMapper(
+			new DefaultLineMapper<Product>(){
+				{
+						setLineTokenizer(new DelimitedLineTokenizer() {
+							{
+								setNames(new String[]{"productId","prodName","productDesc","price","unit"});
+							}
+						});
+						
+						setFieldSetMapper(new BeanWrapperFieldSetMapper<Product>() {
+							{
+								setTargetType(Product.class);
+							}
+						});
+					}
+				}
+		);
+		
+		//step 3 decirle al reader que salte el encabezado
+		reader.setLinesToSkip(1);
+		return reader;
+	}
+	
+	@Bean
 	public Job helloworldJob() {
 		return jobs.get("helloworldJob")
+				/*Realiza un autoincrement en el id del registro del job que se registra en la base de datos*/
 				.incrementer(new RunIdIncrementer())
 				.listener(hwJobExecutionListener)
+				/*Paso inicial*/
 				.start(step1())
+				/*Los pasos siguientes que ejecuta el job 
+				 * NOTA: se pueden agregar cuantos pasos sean necesarios
+				 **/
 				.next(step2())
 				.build();
 	}
