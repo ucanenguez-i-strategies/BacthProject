add24164d2c7a7cfa471e942fe12b881fd2fb3f5
ucanenguez-i-strategies <76168240+ucanenguez-i-strategies@users.noreply.github.com>
2021-11-26 11:39:44 -0600

    Se agregaron autoincrementables para los jobs

--- src/main/java/combacthProject/BatchProject/config/BatchConfiguration.java
+++ src/main/java/combacthProject/BatchProject/config/BatchConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.item.ItemReader;
@@ -74,6 +75,7 @@ public class BatchConfiguration {
 	@Bean
 	public Job helloworldJob() {
 		return jobs.get("helloworldJob")
+				.incrementer(new RunIdIncrementer())
 				.listener(hwJobExecutionListener)
 				.start(step1())
 				.next(step2())
